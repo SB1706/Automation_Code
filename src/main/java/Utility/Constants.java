61c032a9ab5c2df000032198e0cc0f4284c4287a
Sarthak Bansal <sarthak.bansal.773@gmail.com>
2024-06-28 14:02:48 +0530

    Add files via upload

--- src/main/java/Utility/Constants.java
+++ src/main/java/Utility/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static final int wait_2  = 2000;
     public static final int wait_3 = 3000;
     public static final int wait_5 = 5000;
+    public static final int wait_6 = 7000;
     public static final String my_otp = "4130";
 
 
--- src/main/java/Utility/Functions.java
+++ src/main/java/Utility/Functions.java
@@ -1,12 +1,15 @@
 package Utility;
 
 
+import com.beust.ah.A;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,12 +17,19 @@ import java.time.Duration;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import io.appium.java_client.MobileBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.List;
+import java.util.Properties;
 
 
 public class Functions {
     public static AndroidDriver driver;
     static DesiredCapabilities caps;
-    public Functions(AndroidDriver driver) {
+    public  Functions(AndroidDriver driver) {
     }
 
     public static void MyCapabilities() throws MalformedURLException {
@@ -28,7 +38,7 @@ public class Functions {
         caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
         caps.setCapability("version", "12.0");
-        caps.setCapability("deviceName", "RZ8R22R8HMT");
+        caps.setCapability("deviceName", "2C191FDH20027V");
         caps.setCapability("appPackage", "com.otipy.otipy");
         caps.setCapability("appActivity", "com.otipy.otipy.MainActivity");
         caps.setCapability("adbExecTimeout", 60000); // Increase the timeout to 60 seconds
@@ -60,20 +70,28 @@ public class Functions {
 
 
 
-public static void scrollToElement(AppiumDriver driver) {
-    for (int i = 0; i < 5; i++) { // max 5 scrolls
+public static void scrollToElement(AppiumDriver driver, String scroll_elelment) {
+    for (int i = 0; i < 100; i++) { // max 5 scrolls
         try {
-            WebElement element = driver.findElement(MobileBy.xpath("//*[@id=\"btnTapElement\"]"));
-            if (element.isDisplayed()) return;
+
+            WebElement element = Functions.driver.findElement(MobileBy.xpath(scroll_elelment));
+            if (element!=null && element.isDisplayed())
+            {
+                print(scroll_elelment + "This Element Found");
+                print("Stop Scrolling");
+                return;
+            }
+
         } catch (Exception e) {
-            scroll(driver);
+              scroll(driver );
+              print("KeepOn Scrolling because element was not found");
         }
     }
-    System.out.println("Element with text ' not found.");
-}
+        print("Element with text ' not found.");
+    }
 
     public static void scroll (AppiumDriver driver){
-        Dimension dimension = driver.manage().window().getSize();
+        Dimension dimension = Functions.driver.manage().window().getSize();
         int startX = dimension.width / 2;
         int startY = (int) (dimension.height * 0.8);
         int endY = (int) (dimension.height * 0.2);
@@ -87,4 +105,62 @@ public static void scrollToElement(AppiumDriver driver) {
     }
 
 
+    public static void E_mail_Daily_subscription(){
+        // Recipient's email ID needs to be mentioned.
+//        String to = "sarthak.bansal@crofarm.com";
+        String[] to = {
+                "sarthak.bansal@crofarm.com",
+                "deepak.ahlawat@crofarm.com",
+                "sourav.jain@crofarm.com"
+        };
+
+        // Sender's email ID needs to be mentioned
+        String from = "reporting@crofarm.com";
+        final String username = "apikey";  // your Gmail username
+        final String password = "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";// your Gmail password
+        String host = "smtp.sendgrid.net";
+        // Get system properties
+        Properties properties = System.getProperties();
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            for (String recipient : to) {
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            }
+            String UserName = "Sarthak";
+            // Set Subject: header field
+            message.setSubject("Subscription Confirmation");
+            // Now set the actual message
+            String emailContent = "Dear " + UserName + ",\n\nThank you for subscribing to our Daily plan on Otipy!\nYou have successfully subscribed to receive daily deliveries from us.";
+            message.setContent(emailContent, "text/plain; charset=UTF-8");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (AuthenticationFailedException e) {
+            System.err.println("Authentication failed: " + e.getMessage());
+            e.printStackTrace();
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
 }
+
+
